e03325e97e9b2942b7c17324912b01381745b411
singurty <singurty@gmail.com>
2021-07-24 16:23:58 +0545

    init bmalloc

---
+++ ch7/bmalloc.c
@@ -0,0 +1,16 @@
+/* Exercise 7-2 */
+/* bmalloc = better malloc, our malloc
+ * bfree = better free, our free
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+void *bmalloc(size_t size)
+{
+	return 0;
+}
+
+void bfree(void *ptr)
+{
+}
