a21ee6469bc923cdc2f98629adb78a2e24c2bb3e
singurty <singurty@gmail.com>
2021-07-25 09:27:00 +0545

    some progress on making bmalloc use free list

--- ch7/bmalloc.c
+++ ch7/bmalloc.c
@@ -10,19 +10,31 @@
 #include <stdlib.h>
 
 #define MAX_FREES 1000
-#define NEXT_FREE 0
 
+size_t NEXT_FREE = 0;
 void *freeList[MAX_FREES];
 
 void *bmalloc(size_t size)
 {
-	void *programBreak = sbrk(size + sizeof(size_t));
-	/* add metadata before the data segment giving the block form:
-	 * Length of block + Acutal data
-	 */
-	memcpy(programBreak, &size, sizeof(size_t));
-	/* return just the data segment hiding the metadata */
-	return programBreak + sizeof(size_t);
+	/* freeLIst empty, gotta sbrk */
+	if (NEXT_FREE == 0) {
+		void *programBreak = sbrk(size + sizeof(size_t));
+		/* add metadata before the data segment giving the block form:
+		 * Length of block + Acutal data
+		 */
+		memcpy(programBreak, &size, sizeof(size_t));
+		/* return just the data segment hiding the metadata */
+		return programBreak + sizeof(size_t);
+	}
+	size_t currentFree = NEXT_FREE - 1;
+	size_t totalSize = 0;
+	for (; totalSize<(size + sizeof(size_t)); currentFree--) {
+		totalSize += *(size_t *) freeList[currentFree] + sizeof(size_t);
+		printf("totalSize is %zu\n", totalSize);
+	}
+	NEXT_FREE = currentFree - 1;
+	memcpy(freeList[currentFree], &size, sizeof(size_t));
+	return freeList[currentFree] + sizeof(size_t);
 }
 
 void bfree(void *ptr)
@@ -30,18 +42,20 @@ void bfree(void *ptr)
 	ptr = ptr - sizeof(size_t);
 	/* save pointer to free block in freeList */
 	freeList[NEXT_FREE] = ptr;
-	if (NEXT_FREE == 0)
+	if (NEXT_FREE == 0) {
+		++NEXT_FREE;
 		return;
+	}
 	/* store address to previous block and next block in the block itself
 	 * giving the block form: Length of block + Pointer to previous block
 	 * + Pointer to next block + Remaining block
 	 */
 	memcpy(ptr + sizeof(size_t), (void *) freeList[NEXT_FREE-1], sizeof(void *));
 	/* we don't know what the next block will be yet */
-	memcpy(ptr + sizeof(size_t) + sizeof(void ()), (void *) NULL, sizeof(void *));
-	/* update the "next pointer" of previous block */
-	void *prevPtr = freeList[NEXT_FREE-1];
-	memcpy(prevPtr + sizeof(size_t) + sizeof(void ()), (void *) ptr, sizeof(void *));
+//	memcpy(ptr + sizeof(size_t) + sizeof(void ()), (void *) NULL, sizeof(void *));
+	/* update "next pointer" of previous block */
+	memcpy(freeList[NEXT_FREE-1] + sizeof(size_t) + sizeof(void *), (void *) ptr, sizeof(void *));
+	NEXT_FREE++;
 }
 
 int main()
@@ -56,4 +70,9 @@ int main()
 	void *addr4 = bmalloc(memSize);
 	printf("addr4 is %p\n", addr4);
 	bfree(addr);
+	bfree(addr2);
+	bfree(addr3);
+	bfree(addr4);
+	void *newAddr = bmalloc(2048);
+	printf("the new memory of size %zu is at %p\n", *(size_t *) newAddr - sizeof(size_t), newAddr);
 }
