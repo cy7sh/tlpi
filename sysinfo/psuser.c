ca50b36d980bfd9d301335831fdf6c02145e3986
singurty <singurty@gmail.com>
2021-07-31 13:21:50 +0545

    can find pid for a specific user

--- sysinfo/psuser.c
+++ sysinfo/psuser.c
@@ -30,15 +30,18 @@ int main(int argc, char *argv[])
 
 	/* enumerate for directories in /proc */
 	for (struct dirent *rdir = readdir(dir); rdir != NULL; rdir = readdir(dir)) {
+//		printf("checking dir %s\n", rdir->d_name);
 		char *exess;
 		strtol(rdir->d_name, &exess, 10);
 		/* if the directory is not a PID directory */
 		if (exess[0] != '\0') {
+//			printf("not pid\n");
 			continue;
 		}
 		/* read stuff from status file */
-		char filename[strlen("/proc/") + strlen(rdir->d_name) + strlen("/status") + 1];
 		char *pid = rdir->d_name;
+		char filename[strlen("/proc/") + strlen(pid) + strlen("/status") + 1];
+		filename[0] = '\0';
 		strcat(filename, "/proc/");
 		strcat(filename, pid);
 		strcat(filename, "/status");
@@ -46,13 +49,18 @@ int main(int argc, char *argv[])
 		/* the file might not exist now */
 		if (fd == -1)
 			continue;
+//		printf("current file %s\n", filename);
 		/* read from status file and check for match */
 		char buf[65536];
-		for (int readed = read(fd, buf, 65536); readed > 0; readed = read(fd, buf, 65536)) {
-			char searchTerm[strlen("Uid:    ") + sizeof(uidStr)];
-			strcat(searchTerm, "Uid:    ");
+		for (int readed = read(fd, buf, 65535); readed > 0; readed = read(fd, buf, 65536)) {
+			buf[65535] = '\0';
+			char searchTerm[strlen("Uid:\t") + strlen(uidStr) + 2];
+			searchTerm[0] = '\0';
+			strcat(searchTerm, "Uid:\t");
 			strcat(searchTerm, uidStr);
-			if (strstr(buf, uidStr) == NULL) continue;
+//			printf("searching for '%s'\n", searchTerm);
+//			printf("checking PID: %s\n", pid);
+			if (strstr(buf, searchTerm) == NULL) continue;
 			printf("PID: %s\n", pid);
 			break;
 		}
