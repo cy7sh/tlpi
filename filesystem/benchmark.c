fb7e6295de752377d5f037a1325d1c8aa3b3b985
singurty <singurty@gmail.com>
2021-08-08 23:09:56 +0545

    timing file creation seems fine except for random number bug

--- filesystem/benchmark.c
+++ filesystem/benchmark.c
@@ -2,7 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/time.h>
 #include <wordexp.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/times.h>
 
 int main(int argc, char *argv[])
 {
@@ -11,19 +15,36 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	int numFiles = strtol(argv[2], NULL, 10);
+	if (numFiles > 999999) {
+		printf("maximum number of files is 999999\n");
+		exit(EXIT_FAILURE);
+	}
 	wordexp_t directory;
 	wordexp(argv[1], &directory, 0);
+	char filename[strlen(*directory.we_wordv) + 9];
+	filename[0] = '\0';
+	strcat(filename, *directory.we_wordv);
+	strcat(filename, "/");
+	strcat(filename, "x");
+	struct timeval init;
+	struct timeval afterCreate;
+	gettimeofday(&init, NULL);
 	for (int i=0; i<numFiles; i++) {
-		srand(i);
+		srand(time(NULL) + i);
 		int num = rand() % 999999;
 		char numStr[7];
 		sprintf(numStr, "%d", num);
-		char filename[strlen(*directory.we_wordv) + 9];
-		filename[0] = '\0';
-		strcat(filename, *directory.we_wordv);
-		strcat(filename, "/");
-		strcat(filename, "x");
+		filename[strlen(*directory.we_wordv)+2] = '\0';
 		strcat(filename, numStr);
+		int fd = open(filename, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+		if (fd == -1) {
+			perror("file create error");
+			exit(EXIT_FAILURE);
+		}
 		puts(filename);
+		close(fd);
 	}
+	gettimeofday(&afterCreate, NULL);
+	float interval = (afterCreate.tv_usec - init.tv_usec) * 0.000001;
+	printf("time taken: %.03f\n", interval);
 }
