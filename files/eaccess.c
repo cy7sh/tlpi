e7eaf56f7b87e519458b5c691040c546b2940aa7
singurty <singurty@gmail.com>
2021-08-11 16:31:51 +0545

    seems perfect except for ignore supplementary GIDs

--- files/eaccess.c
+++ files/eaccess.c
@@ -17,25 +17,25 @@ int eaccess(const char *pathname, int mode)
 	if (fstat(fd, &filestat) == -1)
 		return -1;
 	if (euid == filestat.st_uid) {
-		if (!((mode & R_OK) & (filestat.st_mode & S_IRUSR)))
+		if ((mode & R_OK) && !(filestat.st_mode & S_IRUSR))
 			return -1;
-		if (!((mode & W_OK) & (filestat.st_mode & S_IWUSR)))
+		if ((mode & W_OK) && !(filestat.st_mode & S_IWUSR))
 			return -1;
-		if (!((mode & X_OK) & (filestat.st_mode & S_IXUSR)))
+		if ((mode & X_OK) && !(filestat.st_mode & S_IXUSR))
 			return -1;
 	} else if (egid == filestat.st_gid) {
-		if (!((mode & R_OK) & (filestat.st_mode & S_IRGRP)))
+		if ((mode & R_OK) && !(filestat.st_mode & S_IRGRP))
 			return -1;
-		if (!((mode & W_OK) & (filestat.st_mode & S_IWGRP)))
+		if ((mode & W_OK) && !(filestat.st_mode & S_IWGRP))
 			return -1;
-		if (!((mode & X_OK) & (filestat.st_mode & S_IXGRP)))
+		if ((mode & X_OK) && !(filestat.st_mode & S_IXGRP))
 			return -1;
 	} else {
-		if (!((mode & R_OK) & (filestat.st_mode & S_IROTH)))
+		if ((mode & R_OK) && !(filestat.st_mode & S_IROTH))
 			return -1;
-		if (!((mode & W_OK) & (filestat.st_mode & S_IWOTH)))
+		if ((mode & W_OK) && !(filestat.st_mode & S_IWOTH))
 			return -1;
-		if (!((mode & X_OK) & (filestat.st_mode & S_IXOTH)))
+		if ((mode & X_OK) && !(filestat.st_mode & S_IXOTH))
 			return -1;
 	}
 	return 0;
@@ -43,5 +43,7 @@ int eaccess(const char *pathname, int mode)
 
 int main()
 {
-
+	int res = eaccess("test", W_OK | X_OK);
+	if (res == 0)
+		printf("allowed\n");
 }
