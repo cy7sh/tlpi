b841a1c891621b2e25e8a7adef12b684f9d5c5c0
singurty <singurty@gmail.com>
2021-08-11 16:10:30 +0545

    should work but we don't check supplementary GIDs

---
+++ files/eaccess.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int eaccess(const char *pathname, int mode)
+{
+	int fd = open(pathname, O_RDONLY);
+	if (fd == -1)
+		return -1;
+	if (mode == F_OK)
+		return 0;
+	int euid = geteuid();
+	int egid = getegid();
+	struct stat filestat;
+	if (fstat(fd, &filestat) == -1)
+		return -1;
+	if (euid == filestat.st_uid) {
+		if (!((mode & R_OK) & (filestat.st_mode & S_IRUSR)))
+			return -1;
+		if (!((mode & W_OK) & (filestat.st_mode & S_IWUSR)))
+			return -1;
+		if (!((mode & X_OK) & (filestat.st_mode & S_IXUSR)))
+			return -1;
+	} else if (egid == filestat.st_gid) {
+		if (!((mode & R_OK) & (filestat.st_mode & S_IRGRP)))
+			return -1;
+		if (!((mode & W_OK) & (filestat.st_mode & S_IWGRP)))
+			return -1;
+		if (!((mode & X_OK) & (filestat.st_mode & S_IXGRP)))
+			return -1;
+	} else {
+		if (!((mode & R_OK) & (filestat.st_mode & S_IROTH)))
+			return -1;
+		if (!((mode & W_OK) & (filestat.st_mode & S_IWOTH)))
+			return -1;
+		if (!((mode & X_OK) & (filestat.st_mode & S_IXOTH)))
+			return -1;
+	}
+	return 0;
+}
+
+int main()
+{
+
+}
