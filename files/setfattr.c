5b01059bbe25f7e57bfc95873e19aac84de56e77
singurty <singurty@gmail.com>
2021-08-12 13:06:07 +0545

    looks perfect

--- files/setfattr.c
+++ files/setfattr.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/xattr.h>
 
 extern char *optarg;
 
@@ -14,11 +15,12 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	int derefer = 1;
+	int rm = 0;
 	char *name = NULL;
 	char *value = NULL;
 	char *pathname = argv[argc-1];
 	char opt;
-	while ((opt = getopt(argc, argv, "hn:v:")) != -1)
+	while ((opt = getopt(argc, argv, "hx:n:v:")) != -1)
 	{
 		switch (opt) {
 		case 'h':
@@ -32,6 +34,35 @@ int main(int argc, char *argv[])
 			value = malloc(strlen(optarg)+1);
 			memcpy(value, optarg, strlen(optarg)+1);
 			break;
+		case 'x':
+			rm = 1;
+			name = malloc(strlen(optarg)+1);
+			memcpy(name, optarg, strlen(optarg)+1);
+			break;
+		}
+	}
+	if (rm == 1) {
+		if (derefer) {
+			if (removexattr(pathname, name) == -1) {
+				perror("failed to remove attribute");
+				exit(EXIT_FAILURE);
+			}
+		}
+		if (!derefer) {
+			if (lremovexattr(pathname, name) == -1) {
+				perror("failed to remove attribute");
+				exit(EXIT_FAILURE);
+			}
+		}
+	} else if (derefer) {
+		if (setxattr(pathname, name, value, sizeof(value), 0) == -1) {
+			perror("failed to set attributes\n");
+			exit(EXIT_FAILURE);
+		}
+	} else {
+		if (lsetxattr(pathname, name, value, sizeof(value), 0) == -1) {
+			perror("failed to set attributes\n");
+			exit(EXIT_FAILURE);
 		}
 	}
 }
