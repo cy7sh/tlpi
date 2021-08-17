dc947190c1013a92bd9b852763208087c8a5d3d1
singurty <singurty@gmail.com>
2021-08-17 11:38:21 +0545

    can resolve . and ..

---
+++ directories/realpath.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <linux/limits.h>
+#include <unistd.h>
+#include <string.h>
+
+char *brealpath(const char *path, char *resolved_path)
+{
+	if (resolved_path == NULL) {
+		resolved_path = malloc(PATH_MAX);
+	}
+	if (path[0] != '/') {
+		char *cwd = malloc(PATH_MAX);
+		if (getcwd(cwd, PATH_MAX) == NULL) {
+			return NULL;
+		}
+		resolved_path = cwd;
+		strcat(resolved_path, "/");
+		strcat(resolved_path, path);
+	} else {
+		strcpy(resolved_path, path);
+	}
+	/* resolve .'s and ..'s */
+	char *slash = strchr(resolved_path, '/');
+	while (slash != NULL) {
+		if (*(slash + 1) == '.') {
+			/* 
+			 * /foo/bar/     .  /  foo/bar/
+			 *         slash +1 +2 +3
+			 * /foo/bar/foo/bar/
+			 *          +1...
+			 */
+			int j = 1;
+			for (char *i = slash+3; *(i-1) != '\0'; i++) {
+				*(slash + j) = *i;
+				j++;
+			}
+		}
+		slash = strchr(slash+1, '/');
+	}
+	return resolved_path;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc > 1) {
+		puts(brealpath(argv[1], NULL));
+	}
+}
